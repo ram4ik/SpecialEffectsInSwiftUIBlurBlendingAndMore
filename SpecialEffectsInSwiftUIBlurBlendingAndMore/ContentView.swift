91aec19750dd8068d42b2d62d4a6a6647fda5596
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-07 12:12:38 +0200

    init

--- SpecialEffectsInSwiftUIBlurBlendingAndMore/ContentView.swift
+++ SpecialEffectsInSwiftUIBlurBlendingAndMore/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var amount: CGFloat = 0.0
+    
     var body: some View {
-        Text("Hello World")
+        
+// Example: 1
+        
+//        ZStack {
+//            Image("img6")
+//
+//            Rectangle()
+//                .fill(Color.orange)
+//                .blendMode(.multiply)
+//        }
+//        .frame(width: 400, height: 500)
+//        .clipped()
+        
+        
+// Example: 2
+        
+//        Image("img6").colorMultiply(.orange)
+
+// Example 3:
+        
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(Color.orange)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
